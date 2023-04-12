cdd81a9d5ea33597d5a3d20da78271c6ca232593
Madelyn Romberg <madelynromberg@g.ucla.edu>
2023-04-12 11:47:08 -0700

    add docstring to sqlForPartialUpdate

--- helpers/sql.js
+++ helpers/sql.js
@@ -1,9 +1,29 @@
 const { BadRequestError } = require("../expressError");
 
-// THIS NEEDS SOME GREAT DOCUMENTATION.
+/**
+ * sqlForPartialUpdate:
+ *
+ * Turns POJO object containing values to update in DB
+ *
+ * Returns an object containing a key with a sanitized database query
+ * and a key of values to insert into database
+ *
+ * ACCEPTS:
+ * {firstName: 'Aliya', age: 32}
+ *
+ * RETURNS:
+ * {
+    setCols: '"first_name"=$1, "age"=$2',
+    values: ["Aliya", 32]
+  }
+ * @param {obj} dataToUpdate
+ * @param {obj} jsToSql
+ */
 
 function sqlForPartialUpdate(dataToUpdate, jsToSql) {
   const keys = Object.keys(dataToUpdate);
+  //ex: keys = ["firstName", "lastName", "password", "email", "isAdmin"]
+
   if (keys.length === 0) throw new BadRequestError("No data");
 
   // {firstName: 'Aliya', age: 32} => ['"first_name"=$1', '"age"=$2']
