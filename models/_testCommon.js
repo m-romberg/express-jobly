2828f888c91186d615faafb7d308e1884a331fc4
Thomas Wilson <tommyhwilson3@gmail.com>
2023-04-14 18:09:41 -0500

    added DELETE jobs table beforeAll tests and INSERT sample data for jobs table

--- models/_testCommon.js
+++ models/_testCommon.js
@@ -9,6 +9,8 @@ async function commonBeforeAll() {
   // noinspection SqlWithoutWhere
   await db.query("DELETE FROM users");
 
+  await db.query("DELETE FROM jobs");
+
   await db.query(`
     INSERT INTO companies(handle, name, num_employees, description, logo_url)
     VALUES ('c1', 'C1', 1, 'Desc1', 'http://c1.img'),
@@ -28,6 +30,13 @@ async function commonBeforeAll() {
         await bcrypt.hash("password1", BCRYPT_WORK_FACTOR),
         await bcrypt.hash("password2", BCRYPT_WORK_FACTOR),
       ]);
+
+  await db.query(`
+        INSERT INTO jobs(title, salary, equity, company_handle)
+        VALUES ('Worker1', '10,000', '0.003', 'c1'),
+              ('Worker2', '12,000', '0.025', 'c1'),
+              ('BigWorker', '45,000', '0.012', 'c3'),
+              ('BigWorker2', '25,000', '0.008, 'c2')`)
 }
 
 async function commonBeforeEach() {
